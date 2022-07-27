aa0160099c9f88044a4408b4845e106c00d973b2
A-sy-coding <tbvjzsk@naver.com>
2022-07-27 21:47:28 +0900

    all connect html & CSS

--- django_an/HealF/urls.py
+++ django_an/HealF/urls.py
@@ -5,6 +5,31 @@ from . import views
 
 app_name = 'HealF'
 urlpatterns = [
+    # --------------- main page --------------
+    
     # /HealF/
-    path('', views.HealFModelView.as_view() , name = 'main'),
+    path('', views.HealFMainView.as_view() , name = 'main'),
+    # /HealF/challenge
+    path('challenge/', views.HealFchallengeView.as_view(), name = 'challenge'),
+    # /HealF/exercise
+    path('exercise/', views.HealFexerciseView.as_view(), name= 'exercise'),
+    # /HealF/community
+    path('community/', views.HealFcommunityView.as_view(), name = 'community'),
+    # /HealF/market
+    path('market/', views.HealFmarketView.as_view(), name='market'),
+    # /HealF/mypage
+    path('mypage/', views.HealFmypageView.as_view(), name='mypage'),
+
+    # --------------- login & sign-up page --------------
+    path('login/', views.HealFloginView.as_view(), name = 'login'),
+    path('sign-up/', views.HealFsignupView.as_view(), name = 'sign-up'),
+
+    # --------------- challenge page --------------
+    path('challenge/exercise/', views.Challenge_exerciseView.as_view(), name = 'challenge_exercise'),
+    path('challenge/diet/', views.Challenge_dietView.as_view(), name = 'challenge_diet'),
+    path('challenge/exercise/squat/', views.ChallengeSquatView.as_view(), name = 'challenge_exercise_squat'),
+
+    # --------------- market page --------------
+    path('market/product/', views.MkarketproductView.as_view(), name = 'market_product'),
+
 ]
--- django_an/HealF/views.py
+++ django_an/HealF/views.py
@@ -3,9 +3,100 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 #- TemplateView 이용 -> template 파일만을 렌더링하는 경우에 사용
-class HealFModelView(TemplateView):
+
+
+# ------------------ main page ------------------------
+
+class HealFMainView(TemplateView):
     template_name = 'main/main.html'
 
     def get(self, request, *args, **kwargs):
         ctx = {}                             # 템플릿에 전달할 데이터
         return self.render_to_response(ctx)
+
+class HealFchallengeView(TemplateView):
+    template_name = 'challenge/challenge.html' # 챌린지 메인 화면
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}                             
+        return self.render_to_response(ctx)
+
+class HealFexerciseView(TemplateView):
+    template_name = 'exercise/exercise.html' # 운동 메인 화면
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}                             
+        return self.render_to_response(ctx)
+
+class HealFcommunityView(TemplateView):
+    template_name = 'community/commu.html' # 커뮤니티 화면
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}                             
+        return self.render_to_response(ctx)
+
+class HealFmarketView(TemplateView):
+    template_name = 'market/market.html' # 마켓 화면
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}                             
+        return self.render_to_response(ctx)
+
+class HealFmypageView(TemplateView):
+    template_name = 'mypage/mypage.html' # 나의정보 화면
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}                             
+        return self.render_to_response(ctx)
+
+
+# ------------------ login & sign-up page ------------------------
+
+class HealFloginView(TemplateView):
+    template_name = 'login/login.html' # login 화면
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}                             
+        return self.render_to_response(ctx)
+
+class HealFsignupView(TemplateView):
+    template_name = 'login/signup.html' # sign-up 화면
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}                             
+        return self.render_to_response(ctx)
+
+
+# ------------------ challenge page ------------------------
+
+class Challenge_exerciseView(TemplateView):
+    template_name = 'challenge/challenge_exercise.html' # challege화면의 운동하기 화면
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}                             
+        return self.render_to_response(ctx)
+
+class Challenge_dietView(TemplateView):
+    template_name = 'challenge/challenge_diet.html' #challenge화면의 다이어트하기 화면
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}                             
+        return self.render_to_response(ctx)
+
+class ChallengeSquatView(TemplateView):
+    template_name = 'challenge/challenge_exercise_squat.html' # challenge 화면의 운동하기 화면의 스쿼드 화면
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}                             
+        return self.render_to_response(ctx)
+
+
+# ------------------ market page ------------------------
+
+class MkarketproductView(TemplateView):
+    template_name = 'categorie/categorie.html' # market 화면의 제품 화면
+
+    def get(self, request, *args, **kwargs):
+        ctx = {}                             
+        return self.render_to_response(ctx)
+
