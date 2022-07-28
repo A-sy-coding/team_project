821e7f9aaff412c6d5f96031e1de104c24c06dbb
unknown <jinus7949@gmail.com>
2022-07-28 22:22:32 +0900

    유저데이터 저장성공

--- django_jin/users/urls.py
+++ django_jin/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+app_name='users'
 urlpatterns = [
     path('login/',views.login, name='login'),
-    path('login/signup/',views.signup),
+    path('login/signup/',views.signup, name='signup'),
+    path('clear/',views.clear, name='clear'),
 ]
